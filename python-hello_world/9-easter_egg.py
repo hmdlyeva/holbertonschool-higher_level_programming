8500d7e18b0a95dc7d15df36e3b0c15aa7a4d97f
hmdlyeva <bd7fcqzu3@code.edu.az>
2024-05-17 15:55:52 +0000

    new line

--- python-hello_world/9-easter_egg.py
+++ python-hello_world/9-easter_egg.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-print("The Zen of Python, by Tim Peters
-
+print("The Zen of Python, by Tim Peters\n
 Beautiful is better than ugly.
 Explicit is better than implicit.
 Simple is better than complex.
