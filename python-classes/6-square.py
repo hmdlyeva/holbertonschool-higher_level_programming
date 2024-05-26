fa08513ffc0efef6e974921bf3b589da9cd44d1a
hmdlyeva <bd7fcqzu3@code.edu.az>
2024-05-26 13:34:34 +0000

    class

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """Define a class Square."""
 
+
 class Square:
      """Represent a square."""
+
     def __init__(self, size=0, position=(0, 0)):
          """Initialize a new square.
 
