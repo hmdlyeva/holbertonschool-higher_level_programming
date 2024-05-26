b8f3bb3b2056a789b92a77aad6eaabdd77cf865f
hmdlyeva <bd7fcqzu3@code.edu.az>
2024-05-26 12:20:45 +0000

    l

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,70 +1,24 @@
 #!/usr/bin/python3
-class Square:
-    def __init__(self, size=0, position=(0, 0)):
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
-        if type(position) is not tuple:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if len(position) != 2:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        for i in position:
-            if not isinstance(i, int) or i < 0:
-                raise TypeError("position must be a tuple of 2 positive "
-                                "integers")
-        self.__position = position
-
-    @property
-    def size(self):
-        return self.__size
-
-    @size.setter
-    def size(self, value):
-        if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        if value < 0:
-            raise ValueError("size must be >= 0")
-        if type(value) is not tuple or len(value) != 2:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        for i in value:
-            if not isinstance(i, int) or i < 0:
-                raise TypeError("position must be a tuple of \
-2 positive "
-                                "integers")
-        self.__position = value
-
-    @property
-    def position(self):
-        return self.__position
-
-    @position.setter
-    def position(self, value):
-        if type(value) is not tuple:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if len(value) != 2:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        for i in value:
-            if not isinstance(i, int) or i < 0:
-                raise TypeError("position must be a tuple of 2 positive "
-                                "integers")
-        self.__position = value
-
-    def area(self):
-        return int(self.__size) ** 2
-
-    def my_print(self):
-        if self.__size == 0:
-            print()
-        else:
-            for i in range(0, self.__size + self.__position[1]):
-                if i < self.__position[1]:
-                    print()
-                    continue
-                for j in range(0, self.__size + self.__position[0]):
-                    if j < self.__position[0]:
-                        print(' ', end='')
-                        continue
-                    print('#', end='')
-                print()
+def roman_to_int(roman_string):
+     if (not isinstance(roman_string, str) or
+            roman_string is None):
+        return (0)
+     roman_dict = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+    }
+    num = 0
+     for i in range(len(roman_string)):
+        if roman_dict.get(roman_string[i], 0) == 0:
+            return (0)
+         if (i != (len(roman_string) - 1) and
+                roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
+                num += roman_dict[roman_string[i]] * -1
+                else:
+            num += roman_dict[roman_string[i]]
+    return (num)
