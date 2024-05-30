da4f4948cb9c78350614057de9961d9cc779c623
hmdlyeva <bd7fcqzu3@code.edu.az>
2024-05-30 18:26:33 +0000

    last

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""
+This is the addition module.
+it adds 2 integers
+a and b must be first casted
+"""
 def add_integer(a, b=98):
+     """a and are integers
+    Returns an integer: the addition of a and b
+    """
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
     if not isinstance(b, int) and not isinstance(b, float):
--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+"""
+Say My Name - a function that says your name.
+"""
 def say_my_name(first_name, last_name=""):
+     """
+    Says your name.
+    first_name and last_name must be strings otherwise, will raise a TypeError
+    """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
