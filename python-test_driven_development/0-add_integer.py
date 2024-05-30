51f58a4ab92441752620418bfbb6a40c4bc442d6
hmdlyeva <bd7fcqzu3@code.edu.az>
2024-05-30 18:28:46 +0000

    last

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -4,6 +4,8 @@ This is the addition module.
 it adds 2 integers
 a and b must be first casted
 """
+
+
 def add_integer(a, b=98):
      """a and are integers
     Returns an integer: the addition of a and b
