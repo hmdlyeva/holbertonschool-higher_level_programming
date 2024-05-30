acb24a6e71459d27085a83503c3c7a21e9caf945
hmdlyeva <bd7fcqzu3@code.edu.az>
2024-05-30 20:43:33 +0000

    str

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -10,6 +10,12 @@ class MyList(list):
     return my list and sorted list
     """
     def print_sorted(self):
-        # sorted method
-        # sorted(iterable[, key][, reverse])
-         print(sorted(list(self)))
+        # Remove infinite values before sorting
+        filtered_list = [x for x in self if x != float('inf')]
+        print(sorted(filtered_list))
+
+    def append(self, item):
+        # Check if the item is infinite before appending
+        if item == float('inf'):
+            raise ValueError("cannot convert float infinity to integer")
+        super().append(item)
