5dce6c59b4af1cbf4825efdf6446521c0db9c9d1
hmdlyeva <bd7fcqzu3@code.edu.az>
2024-05-30 18:32:03 +0000

    test

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -10,9 +10,9 @@ def say_my_name(first_name, last_name=""):
     first_name and last_name must be strings otherwise, will raise a TypeError
     """
     # first_name must be an integer
-    if not isinstance(first_name, str):
+     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     # last_name must be an integer
-    if not isinstance(last_name, str):
+     if type(last_name) is not str:
         raise TypeError("last_name must be a string")
     print("My name is {:s} {:s}".format(first_name, last_name))
