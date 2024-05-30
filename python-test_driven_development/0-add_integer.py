4782ce1d1dc01708ee7ce73a9e26654367412ec5
hmdlyeva <bd7fcqzu3@code.edu.az>
2024-05-30 18:34:16 +0000

    test

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -7,15 +7,15 @@ a and b must be first casted
 
 
 def add_integer(a, b=98):
-     """a and are integers
+    """a and are integers
     Returns an integer: the addition of a and b
     """
-    if not isinstance(a, int) and not isinstance(a, float):
-        raise TypeError("a must be an integer")
-    if not isinstance(b, int) and not isinstance(b, float):
-        raise TypeError("b must be an integer")
     if isinstance(a, float):
         a = int(a)
     if isinstance(b, float):
         b = int(b)
+    if not (isinstance(a, int)):
+        raise TypeError("a must be an integer")
+    if not (isinstance(b, int)):
+        raise TypeError("b must be an integer")
     return (a + b)
