8a4fe591edf2a3220c580b4d96e72ce1844e0caa
hmdlyeva <bd7fcqzu3@code.edu.az>
2024-05-30 19:50:40 +0000

    tasks

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-Rectangle = __import__('9-rectangle').Rectangle
 """
 ===================================
 module with class BaseGeometry
 ===================================
 """
 
-
+Rectangle = __import__('9-rectangle').Rectangle
 class Square(Rectangle):
     """Square class that inherits from Rectangle that inherits BaseGeometry"""
 
