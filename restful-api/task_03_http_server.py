566e2e39ac0f7a95ea3df305d1e91407c96ef37a
unknown <bd7fcqzu3@code.edu.az>
2024-06-09 15:45:40 +0400

    restful api

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 import http.server
-import json
 
 class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
@@ -14,8 +13,7 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header('Content-type', 'application/json')
             self.end_headers()
-            data = {"name": "John", "age": 30, "city": "New York"}
-            self.wfile.write(json.dumps(data).encode('utf-8'))
+            self.wfile.write(b'{"name": "John", "age": 30, "city": "New York"}')
         elif self.path == '/status':
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
