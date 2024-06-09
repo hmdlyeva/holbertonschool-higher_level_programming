aacb9d01e45bf408f6b72c16f23dafd719c8a145
unknown <bd7fcqzu3@code.edu.az>
2024-06-09 16:57:25 +0400

    restful api

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -11,10 +11,7 @@ auth = HTTPBasicAuth()
 jwt = JWTManager(app)
 
 # User data
-users = {
-    "user1": {"username": "user1", "password": generate_password_hash("password"), "role": "user"},
-    "admin1": {"username": "admin1", "password": generate_password_hash("admin_password"), "role": "admin"}
-}
+users = {}
 
 # Basic authentication
 @auth.verify_password
