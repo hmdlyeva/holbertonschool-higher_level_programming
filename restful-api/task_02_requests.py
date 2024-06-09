622f788d09661a30c9ec8dd4b733ebeb043b5b5d
unknown <bd7fcqzu3@code.edu.az>
2024-06-09 15:28:53 +0400

    restful api

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+import requests
+import csv
+
+def fetch_and_print_posts():
+    url = 'https://jsonplaceholder.typicode.com/posts'
+    response = requests.get(url)
+    print(f'Status Code: {response.status_code}')
+    
+    if response.status_code == 200:
+        posts = response.json()
+        for post in posts:
+            print(post['title'])
+
+def fetch_and_save_posts():
+    url = 'https://jsonplaceholder.typicode.com/posts'
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        posts = response.json()
+        formatted_posts = [{'id': post['id'], 'title': post['title'], 'body': post['body']} for post in posts]
+        
+        with open('posts.csv', 'w', newline='') as csvfile:
+            fieldnames = ['id', 'title', 'body']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            
+            writer.writeheader()
+            writer.writerows(formatted_posts)
+
+if __name__ == "__main__":
+    fetch_and_print_posts()
+    fetch_and_save_posts()
