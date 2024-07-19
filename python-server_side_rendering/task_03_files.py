cd02ad6c94769072041cda9f01ac4df8e2ef6ae9
hmdlyeva <bd7fcqzu3@code.edu.az>
2024-07-19 19:42:15 +0400

    task 03

--- python-server_side_rendering/task_03_files.py
+++ python-server_side_rendering/task_03_files.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
-import pandas as pd
+from read_json import read, read_csv, read_json
+
 
 app = Flask(__name__)
 
@@ -9,17 +10,18 @@ def home():
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+      return render_template('about.html')
 
 @app.route('/contact')
 def contact():
-    return render_template('contact.html')
+      return render_template('contact.html')
 
 @app.route('/items')
 def items():
-    file_name = "items.json"
-    items = read_json(file_name)
-    return render_template('items.html', items=items)
+      
+      file_name = "items.json"
+      items = read(file_name)
+      return render_template('items.html', items=items)
 
 @app.route('/products')
 def display_products():
@@ -27,9 +29,9 @@ def display_products():
     product_id = request.args.get('id')
 
     if source == 'json':
-        products = read_json('products.json')
+        products = read_json()
     elif source == 'csv':
-        products = read_csv('products.csv')
+        products = read_csv()
     else:
         return render_template('product_display.html', error="Wrong source")
     
@@ -37,22 +39,8 @@ def display_products():
         products = [product for product in products if str(product['id']) == product_id]
         if not products:
             return render_template('product_display.html', error="Product not found")
-    
+	
     return render_template('product_display.html', products=products)
 
-def read_json(file_path):
-    try:
-        return pd.read_json(file_path).to_dict(orient='records')
-    except Exception as e:
-        print(f"Error reading JSON file: {e}")
-        return []
-
-def read_csv(file_path):
-    try:
-        return pd.read_csv(file_path).to_dict(orient='records')
-    except Exception as e:
-        print(f"Error reading CSV file: {e}")
-        return []
-
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+       app.run(debug=True, port=5000)
